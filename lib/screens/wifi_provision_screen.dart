09d6c22ed3b987432e759e143b7338203d469e8f
Abdi Mk <abdimk@192.168.1.5>
2026-07-22 17:53:12 +0300

    Fix iOS WiFi SSID detection: add wifi-info entitlement + request location permission on iOS

--- lib/screens/wifi_provision_screen.dart
+++ lib/screens/wifi_provision_screen.dart
@@ -127,6 +127,8 @@ class _WifiProvisionScreenState extends State<WifiProvisionScreen> {
           if (!loc.isGranted && !loc.isLimited) {
             await PermissionGate.enqueueLocationCoarse();
           }
+        } else {
+          await PermissionGate.locationWhenInUse();
         }
       } catch (_) {}
       final info = await _nativeBleMethod.invokeMethod<Map<dynamic, dynamic>>('getWifiInfo');
